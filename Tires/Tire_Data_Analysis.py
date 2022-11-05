c85d1257f1fd0135d3d008ee487eaa409df10ed9
orion-miller <33074666+orion-miller@users.noreply.github.com>
2022-11-04 22:25:47 -0400

    add plots for other datasets

--- Tires/Tire_Data_Analysis.py
+++ Tires/Tire_Data_Analysis.py
@@ -13,5 +13,17 @@ df_FY_IA_Dep = pd.read_csv("Front_Tire - FY_IA_Dep.csv")
 df_FX_IA_Dep = pd.read_csv("Front_Tire - FX_IA_Dep.csv")
 df_Friction_Circle = pd.read_csv("Front_Tire - Friction_Circle.csv")
 
-fig_SA_Curve = px.line(df_SA_Curve,x='SA',y='FFact',template="plotly_dark",title="Relative Mu vs. Slip Angle",width=600, height=400)
+fig_SA_Curve = px.line(df_SA_Curve,x='SA (deg)',y='FFact (-)',template="plotly_dark",title="Relative Mu-Y vs. Slip Angle",width=600, height=400)
 fig_SA_Curve.show()
+
+fig_Load_Dep = px.line(df_Load_Dep,x='Fz (kgf)',y='Fy (kgf)',template="plotly_dark",title="Relative Mu-Y vs. Load",width=600, height=400)
+fig_Load_Dep.show()
+
+fig_FY_IA_Dep = px.line(df_FY_IA_Dep,x='Camber (deg)',y='FFact (-)',template="plotly_dark",title="Relative Mu-Y vs. Camber",width=600, height=400)
+fig_FY_IA_Dep.show()
+
+fig_FX_IA_Dep = px.line(df_FX_IA_Dep,x='Camber (deg)',y='FFact (-)',template="plotly_dark",title="Relative Mu-X vs. Camber",width=600, height=400)
+fig_FX_IA_Dep.show()
+
+fig_Friction_Circle = px.line(df_Friction_Circle,x='Fy (kgf)',y='Fx (kgf)',template="plotly_dark",title="Traction Circle",width=600, height=400)
+fig_Friction_Circle.show()
